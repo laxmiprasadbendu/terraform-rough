0ecf71e6792e6d4c75b5f438d0d0529881781c64
laxmiprasadbendu <87857782+laxmiprasadbendu@users.noreply.github.com>
2021-12-29 08:49:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "aws" {
   access_key = "AKIAYH5NRM4UZBY6FZOD"
   secret_key = "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"
 }
+resource "aws_s3_bucket" "b" {
+  bucket = "laxmi-29-12-21"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
